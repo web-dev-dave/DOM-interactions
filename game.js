ad27f5c65144b7dc3abe23615785612a8ffc8326
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-09-15 15:04:45 +1200

    make dot counter'

--- game.js
+++ game.js
@@ -1,45 +1,61 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
+function start() {
   bindEventListeners(document.getElementsByClassName('board')[0].children)
 }
 
-function bindEventListeners (dots) {
+function bindEventListeners(dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue);
+    dots[i].addEventListener('dblclick', hide);
   }
 }
 
-function makeGreen (evt) {
+
+function makeGreen(evt) {
   evt.preventDefault()
   evt.target.classList.toggle('green')
   updateCounts()
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue(evt) {
+  evt.target.classList.toggle('blue');
 
+  updateCounts();
+}
 // CREATE FUNCTION hide HERE
+function hide(evt) {
+  evt.target.classList.toggle('invisible');
 
-function updateCounts () {
+  updateCounts();
+}
+
+function updateCounts() {
   var totals = {
     blue: 0,
     green: 0,
-    invisible: 0
+    invisible: 0,
   }
-  
+
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+totals.blue = document.getElementsByClassName('blue').length;
+totals.green = document.getElementsByClassName('green').length;
+totals.invisible = document.getElementsByClassName('invisible').length;
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
 
-function displayTotals (totals) {
+function displayTotals(totals) {
   for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
+    document.getElementById(key + '-total').innerHTML = totals[key];
+    console.log(totals);
   }
 }
